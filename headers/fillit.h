c1635872cffd47498378dc7eb5c08fb9253b04dc
Greenbeard Geordi <ggeordi@at-i5.21-school.ru>
2019-12-15 17:30:02 +0300

    Refactoring

--- headers/fillit.h
+++ headers/fillit.h
@@ -6,7 +6,7 @@
 /*   By: ggeordi <ggeordi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/15 15:50:18 by ggeordi           #+#    #+#             */
-/*   Updated: 2019/12/15 17:21:33 by ggeordi          ###   ########.fr       */
+/*   Updated: 2019/12/15 17:26:33 by ggeordi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 #include "libft.h"
 #include "get_next_line.h"
 
-typedef struct		s_tetromino
+typedef struct			s_tetromino
 {
-	char			*body;
-	char			letter;
-}					t_tetromino;
+	char				*body;
+	char				letter;
+}						t_tetromino;
 
-int					read_tetrominoes(char *filename);
+t_list					**read_tetrominoes(int fd);
 
 #endif
